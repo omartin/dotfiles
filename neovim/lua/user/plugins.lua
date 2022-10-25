9a2ace8dfa4b56b5289f533fab962939dcef26ad
Olivier Martin <olivier.martin@govirtuo.com>
2022-10-25 15:47:36 +0200

    wip

--- neovim/lua/user/plugins.lua
+++ neovim/lua/user/plugins.lua
@@ -105,12 +105,6 @@ return packer.startup(function(use)
   -- tab
   use "alvarosevilla95/luatab.nvim"
 
-  -- aerial
-  use { 
-    "stevearc/aerial.nvim",
-    config = function() require("aerial").setup() end,
-  }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
