f1eaf4c921005a3ff2a7af103ddb5fee5fee43e9
Olivier Martin <olivier.martin@govirtuo.com>
2023-03-11 18:41:25 +0100

    update lsp-zero config

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,29 +1,38 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
     dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' }, -- Required
+      { 'williamboman/mason.nvim' }, -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' }, -- Optional
+      { 'hrsh7th/cmp-path' }, -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
       -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+      { 'L3MON4D3/LuaSnip' }, -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
     },
-    config = 
-      function()
-        local lsp = require("lsp-zero")
-        lsp.preset("recommended")
-        lsp.setup()
-      end,
+    config =
+    function()
+      local lsp = require('lsp-zero').preset({
+        name = 'recommended',
+        set_lsp_keymaps = true,
+        manage_nvim_cmp = true,
+        suggest_lsp_servers = true,
+      })
+
+      -- (Optional) Configure lua language server for neovim
+      lsp.nvim_workspace()
+
+      lsp.setup()
+    end,
   }
 }
