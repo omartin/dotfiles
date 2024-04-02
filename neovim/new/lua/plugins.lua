d7763ce71e3c62d3c7d027f1196b3b9167f79bac
Olivier Martin <olivier.martin@govirtuo.com>
2024-04-02 16:13:03 +0200

    wip

--- neovim/new/lua/plugins.lua
+++ neovim/new/lua/plugins.lua
@@ -21,28 +21,12 @@ require("lazy").setup({
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects", 
+    },
     config = function ()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          'c',
-          'lua',
-          'bash',
-          'html',
-          'javascript',
-          'json',
-          'lua',
-          'markdown',
-          'markdown_inline',
-          'python',
-          'query',
-          'regex',
-          'tsx',
-          'typescript',
-          'vim',
-          'vimdoc',
-          'yaml',
-        },
-
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "javascript", "typescript", "yaml" },
         auto_install = true,
 
         highlight = {
@@ -62,42 +46,19 @@ require("lazy").setup({
         textobjects = {
           select = {
             enable = true,
-
-            -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
-
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ["af"] = "@function.outer",
               ["if"] = "@function.inner",
               ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
               ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
             selection_modes = {
               ['@parameter.outer'] = 'v', -- charwise
               ['@function.outer'] = 'V', -- linewise
               ['@class.outer'] = '<c-v>', -- blockwise
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
             include_surrounding_whitespace = true,
           },
         },
@@ -105,6 +66,10 @@ require("lazy").setup({
     end,
   },
   {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lsp = require("lspconfig")
+      lsp.tsserver.setup({})
+    end,
+  }
 })
