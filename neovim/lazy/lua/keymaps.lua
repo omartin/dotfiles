670bf8e1b007093c07bd6eb1664c79a995becb0a
Olivier Martin <olivier.martin@govirtuo.com>
2024-04-27 19:06:22 +0200

    Improve keymap desc

--- neovim/lazy/lua/keymaps.lua
+++ neovim/lazy/lua/keymaps.lua
@@ -1,10 +1,10 @@
-local opts = { noremap = true, silent = true }
-
 -- Shorten function name
-local keymap = vim.api.nvim_set_keymap
+local function keymap (mode, lhs, rhs, desc)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { desc = desc, noremap = true, silent = true })
+end
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -18,32 +18,32 @@ vim.g.maplocalleader = " "
 
 -- NORMAL --
 -- Splits
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-keymap("n", "<Leader>-", "<C-W>s", opts)
-keymap("n", "<Leader>|", "<C-W>v", opts)
-keymap("n", "<Up>", ":horizontal resize +2<CR>", opts)
-keymap("n", "<Down>", ":horizontal resize -2<CR>", opts)
-keymap("n", "<Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-h>", "<C-w>h", 'Left')
+keymap("n", "<C-j>", "<C-w>j", 'Down')
+keymap("n", "<C-k>", "<C-w>k", 'Up')
+keymap("n", "<C-l>", "<C-w>l", 'Right')
+keymap("n", "<Leader>-", "<C-W>s", 'Split vertical')
+keymap("n", "<Leader>|", "<C-W>v", 'Split horizontal')
+keymap("n", "<Up>", ":horizontal resize +2<CR>", 'Resize horizontal +')
+keymap("n", "<Down>", ":horizontal resize -2<CR>", 'Resize horizontal -')
+keymap("n", "<Left>", ":vertical resize +2<CR>", 'Resize vertical +')
+keymap("n", "<Right>", ":vertical resize -2<CR>", 'Resize vertical -')
 
 -- Hide search highlight
-keymap("n", "<Esc><Esc>", "<Esc>:nohlsearch<CR><Esc>", opts)
+keymap("n", "<Esc><Esc>", "<Esc>:nohlsearch<CR><Esc>", 'Hide highlight')
 
 -- Buffers & Tabs
-keymap("n", "<Leader><Leader>", "<c-^>", opts)
-keymap("n", "<Tab>", ":bnext<CR>", opts)
-keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
-keymap("n", "<Leader><Tab>", ":tabnext<CR>", opts)
-keymap("n", "<Leader><S-Tab>", ":tabprevious<CR>", opts)
-keymap("n", "<Leader>x", ":bdelete<CR>", opts)
+keymap("n", "<Leader><Leader>", "<c-^>", 'Previous buffer')
+keymap("n", "<Tab>", ":bnext<CR>", 'Next buffer')
+keymap("n", "<S-Tab>", ":bprevious<CR>", 'Previous buffer')
+keymap("n", "<Leader><Tab>", ":tabnext<CR>", 'Next tab')
+keymap("n", "<Leader><S-Tab>", ":tabprevious<CR>", 'Previous tab')
+keymap("n", "<Leader>x", ":bdelete<CR>", 'Delete buffer')
 
 -- Comments
-keymap("n", "<Leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap("v", "<Leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", opts)
+keymap("n", "<Leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", 'Comment')
+keymap("v", "<Leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", 'Comment')
 
 
 -- TERMINAL --
-keymap("t", "<Esc><Esc>", "<C-\\><C-n>", opts)
+keymap("t", "<Esc><Esc>", "<C-\\><C-n>", 'Echap')
