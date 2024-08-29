cf0b0ed50709a9b38f99e55f38c3c48948276567
Olivier Martin <olivier.martin@govirtuo.com>
2024-08-29 17:19:32 +0200

    fix which-key

--- neovim/lazy/lua/plugins/which-key.lua
+++ neovim/lazy/lua/plugins/which-key.lua
@@ -13,23 +13,16 @@ return {
   config = function(_, opts)
     local wk = require('which-key')
     wk.setup(opts)
-    wk.register({
-      ['<Leader>f'] = {
-        name = ' Find',
-        f = { '<cmd>Telescope find_files<cr>', 'Find files' },
-        w = { '<cmd>Telescope live_grep<cr>', 'Live grep' },
-        t = { '<cmd>Telescope colorscheme<cr>', 'Find colorschemes' },
-        o = { '<cmd>Telescope oldfiles<cr>', 'Old files' },
-        k = { '<cmd>Telescope keymaps<cr>', 'Keymaps' },
-      },
-      ['<Leader>g'] = {
-        name = '󰊢 Git',
-        b = { '<cmd>Gitsigns blame_line<cr>', 'Blame line' },
-        s = { '<cmd>Telescope git_status<cr>', 'Git status' },
-      },
-      ['<Leader>o'] = {
-        name = ' Ollama',
-      },
+    wk.add({
+      { "<Leader>f", group = "Find" },
+      { "<Leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<Leader>fk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+      { "<Leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Old files" },
+      { "<Leader>ft", "<cmd>Telescope colorscheme<cr>", desc = "Find colorschemes" },
+      { "<Leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<Leader>g", group = "Git" },
+      { "<Leader>gb", "<cmd>Gitsigns blame_line<cr>", desc = "Blame line" },
+      { "<Leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
     })
   end,
 }
