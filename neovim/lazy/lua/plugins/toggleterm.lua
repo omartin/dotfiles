e045e7ddd3921e03c2e18e7833edf2788c1e4442
Olivier Martin <olivier.martin@govirtuo.com>
2025-05-15 10:42:47 +0200

    improve toggleterm keymaps

--- neovim/lazy/lua/plugins/toggleterm.lua
+++ neovim/lazy/lua/plugins/toggleterm.lua
@@ -2,9 +2,10 @@ return {
   'akinsho/toggleterm.nvim',
   event = 'VeryLazy',
   keys = {
-    { '<C-t>', '<cmd>ToggleTerm direction=float<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
-    { '<Leader>tr', '<cmd>ToggleTerm direction=vertical size=80<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
-    { '<Leader>tb', '<cmd>ToggleTerm<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
+    { '<C-t>', '<cmd>ToggleTerm<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
+    { '<Leader>tf', '<cmd>ToggleTerm direction=float<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
+    { '<Leader>tv', '<cmd>ToggleTerm direction=vertical size=80<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
+    { '<Leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', mode = { 'n', 't' }, desc = 'Toggle Terminal' },
   },
   opts = {},
 }
